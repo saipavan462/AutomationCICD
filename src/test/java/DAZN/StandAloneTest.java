bff4f4ef8c5a2d0afec2fb45f636eee118abc9d5
saipavanu1998 <saipavan.uppalapati@dazn.com>
2024-04-05 09:35:18 +0530

    Initial files commit

---
+++ src/test/java/DAZN/StandAloneTest.java
@@ -0,0 +1,24 @@
+package DAZN;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class StandAloneTest {
+
+	public static void main(String[] args) 
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		List<WebElement> products = driver.findElements(By.className(".mb-3"));
+		
+	}
+
+}
